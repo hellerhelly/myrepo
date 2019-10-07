2695e11cae58d38a8a9126e44aa76530d0558bea
hellerhelly <hellerhelly@gmail.com>
2019-10-07 13:19:09 +0000

    Kommentar hinzugefügt

--- hallo.sh
+++ hallo.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+# hallo.sh gibt den Benutzer, den Scriptnamen, Datum und Uhrzeit
+# sowie das arbeitsverzeichnis aus
 echo "$USER hat $0 am $(date +"%d.%m.%Y") um $(date +"%H:%m") im Verzeichnis $PWD aufgerufen"
